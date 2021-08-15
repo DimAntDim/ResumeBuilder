c4392b178a898342501b7ab99fda24a5cb697e7b
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2021-08-15 22:53:17 +0300

    create user template creation data motel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,28 @@
+
+from .models import Education, Employment_history, Languages, Skills, User_Templates
 from accounts.models import Profile
 from django.contrib import admin
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User_Templates)
+class User_TemplatesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Languages)
+class LanguagesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Skills)
+class SkillsAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Employment_history)
+class Employment_historyAdmin(admin.ModelAdmin):
     pass
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from django.db.models.fields.related import ForeignKey
 from template_manager.models import Template
 from account_auth.models import CustomUser
 from django.db import models
@@ -30,11 +31,63 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+class Skills(models.Model):
+    name = models.CharField(
+        max_length=60,
+        null=True,
+        blank=True,
+    )
+
+class Languages(models.Model):
+    language = models.CharField(
+        max_length=60,
+        null=True,
+        blank=True,
+    )
+
+class Employment_history(models.Model):
+    company_name = models.CharField(
+        max_length=60,
+        null=True,
+        blank=True,
+    )
+    position = models.CharField(
+        max_length=60,
+        blank=True,
+        null=True,
+    )
+    start = models.DateField()
+    end = models.DateField()
+    description = models.TextField(
+        blank=True,
+    )
+
+class Education(models.Model):
+    school_name = models.CharField(
+        max_length=60,
+        null=True,
+    )
+    degree = models.CharField(
+        max_length=50,
+        null=True,
+    )
+    school_name = models.CharField(
+        max_length=60,
+        null=True,
+        )
+    start = models.DateField()
+    end = models.DateField()
+
 
 class User_Templates(models.Model):
     template = models.ForeignKey(
         Template,
         on_delete=models.CASCADE,
+    )
+    photo = models.ImageField(
+        upload_to = 'profiles/photo/',
+        blank=True,
+        null=True,
     )
     first_name = models.CharField(
         max_length=20,
@@ -63,9 +116,25 @@ class User_Templates(models.Model):
     phone = models.IntegerField()
     contact_email = models.EmailField
     about_me = models.TextField()
-    employment_history = models.TextField()
-    education = models.TextField()
-    skills = models.TextField()
+
+    employment_history = models.ForeignKey(
+        Employment_history,
+        on_delete=models.CASCADE,
+    )
+    education = models.ForeignKey(
+        Education,
+        on_delete=models.CASCADE,
+    )
+    skills = models.ForeignKey(
+        Skills,
+        on_delete=models.CASCADE,
+        )
+    languages = models.ForeignKey(
+        Languages,
+        on_delete=models.CASCADE,
+    )
+
+
 
 
 from account_auth.signals import *
