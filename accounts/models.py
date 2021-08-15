63043d8377bcab5d18e25903afbd938f1171ba26
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2021-08-15 21:42:52 +0300

    user_template model

--- accounts/models.py
+++ accounts/models.py
@@ -31,10 +31,41 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.email
 
-class Templates(models.Model):
+class User_Templates(models.Model):
     template = models.ForeignKey(
         Template,
         on_delete=models.CASCADE,
     )
+    first_name = models.CharField(
+        max_length=20,
+        null=True,
+    )
+    last_name =models.CharField(
+        max_length=20,
+        null=True,
+    )
+    occupation = models.CharField(
+        max_length=30,
+        null=True,
+    )
+    address = models.CharField(
+        max_length=20,
+        null=True,
+    )
+    city = models.CharField(
+        max_length=20,
+        null=True,
+    )
+    country = models.CharField(
+        max_length=20,
+        null=True,
+    )
+    phone = models.IntegerField()
+    contact_email = models.EmailField
+    about_me = models.TextField()
+    employment_history = models.TextField()
+    education = models.TextField()
+    skills = models.TextField()
+
 
 from account_auth.signals import *
